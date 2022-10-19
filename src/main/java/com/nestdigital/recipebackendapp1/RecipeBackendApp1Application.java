4bbe5e6d28e88d3ffb4078a47585562c034febef
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-19 21:25:34 +0530

    recipe-backend-app

---
+++ src/main/java/com/nestdigital/recipebackendapp1/RecipeBackendApp1Application.java
@@ -0,0 +1,13 @@
+package com.nestdigital.recipebackendapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBackendApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBackendApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/recipebackendapp1/RecipeBackendApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.recipebackendapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBackendApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
